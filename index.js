90b2b4037d78489ed6118ad753ff91c6359ceda9
Soldy <4786022+Soldy@users.noreply.github.com>
2019-10-27 09:27:01 +0000

    mariadb
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,48 @@
+const mariadb = require('mariadb');
+
+
+exports.mariadbrc = function(){
+    this.setConfig = function (inputConfig){
+        for (let i in inputConfig)
+             if (typeof config[i] !== "undefined")
+                 config[i] = inputConfig[i];
+        return true;
+    }
+    this.connect=async function(){
+         pool = await mariadb.createPool(config);
+         return connection = await pool.getConnection();
+    }
+    this.disconnect= async function(){
+
+    },
+    this.functionQuery= async function(func, varArray){
+        return await this.qf(func, varArray);
+    }
+    this.qf = async function(func, varArray){
+        return await conection.query(
+            "SELECT `"+func+"`('"+varArray+"') AS `id`;"
+        );
+    }
+    this.procedureQuery= async function(procedure, varArray){
+        return await this.qp(procedure, varArray);
+    }
+    this.qp = async function(procedure, varArray){
+        return await conection.query(
+            "CALL `"+procedure+"`('"+varArray+"') AS `id`;"
+        );
+    }
+    let status      = 0;
+    let connetction = "";
+    let pool = async function(){
+        return false;
+    }
+    let config = {
+        host            : '127.0.0.1',
+        user            : 'root',
+        password        : '123456',
+        database        : 'mails',
+        connectionLimit : 5
+    }
+
+}
+
